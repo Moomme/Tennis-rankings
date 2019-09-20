d185d43cb81deaf5c3e089f7b04ed9718be20e17
mohammad <Moomme>
2019-09-20 18:01:57 +0200

    formatted dict to html so it's readable on site

--- flasksite.py
+++ flasksite.py
@@ -1,20 +1,25 @@
 from flask import Flask, escape, request
 
 app = Flask(__name__)
+
 @app.route('/')
-def hello():
-    name = request.args.get("name", "Mohammad")
-    return f'<h1>Hello, {escape(name)}!<h1>'
+def first():
+    return "<h1>First page<h1>"
 
 @app.route('/second')
 def second():
-    name = request.args.get("name", "Mohammad 2")
-    return f'<h1>Hello, {escape(name)}!<h1>'
+    return '<h1>First page<h1>'
 
 @app.route('/tennis')
 def tennis():
     # name = request.args.get("name", "Mohammad 2")
-    dict = eval(open("db.txt", 'r').read())
-    return str(dict)
+    rankings = eval(open("db.txt", 'r').read())
+    retStr = ""
+    lenR = len(rankings)
+    for i in range(lenR):
+        player_dict = rankings[i]
+        retStr += "<p>" + str(i+1) + ". Name: " + player_dict["Name"] + ", Age: " + player_dict["Age"] + ", Points: " + player_dict["Points"] + ", Tournaments Played: " + player_dict["Tourns Played"] + " </p>"
+    return  retStr + str(rankings)
+
 if __name__ == '__main__':
     app.run(debug=True)
