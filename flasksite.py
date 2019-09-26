e2c4302f1479e3eb3d2e4cbf12efd3cc63d46225
Aleksandar Mitic <mitic.alex@hotmail.se>
2019-09-26 20:40:27 +0200

    Added scheduler for scrapers

--- flasksite.py
+++ flasksite.py
@@ -1,7 +1,12 @@
 from flask import Flask, escape, request, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+from scraper import atp_rankings, wta_rankings, itf_rankings
 import json
 
 app = Flask(__name__)
+
+# This should be weekly in prod as rankings change weekly 
+SCRAPER_INTERVAL = 10
 
 @app.route('/')
 def first():
@@ -14,4 +19,12 @@ def tennis():
     return  render_template('rankings.html', rankings=rankings, system='ATP')
 
 if __name__ == '__main__':
+    # In debug mode flask initilizes the app twice and therefore each job will be registered twice. 
+    # This will not be a problem in prod
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(atp_rankings, 'interval', seconds=SCRAPER_INTERVAL)
+    scheduler.add_job(wta_rankings, 'interval', seconds=SCRAPER_INTERVAL)
+    scheduler.add_job(itf_rankings, 'interval', seconds=SCRAPER_INTERVAL)
+    scheduler.start()
+
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -2,28 +2,36 @@ from selenium import webdriver
 import html2text
 import json
 
-driver = webdriver.Firefox(executable_path='/home/mohammad/Downloads/geckodriver')
+driver = webdriver.Firefox(executable_path='/home/Alex/Downloads/geckodriver')
 driver.get("https://www.atptour.com/en/rankings/singles")
-data = driver.find_element_by_class_name('mega-table').get_attribute('innerHTML')
 
-#print("HELLO OOOO " + str(data))
-h = html2text.HTML2Text()
-h.ignore_links = True
+def atp_rankings(): 
+    data = driver.find_element_by_class_name('mega-table').get_attribute('innerHTML')
 
-print(type(data))
-l = h.handle(data).split('|')
-l = [x for x in l if "/" not in x and "\n" not in x]
-print(l)
-print(len(l))
-rankings = [{}]*100
-rank = 0
-lenl = len(l)
-print(lenl)
-for i in range(0, lenl, 5):
-    print(i)
-    rankings[rank] = {"Name": str(l[i+0]).strip(), "Age": str(l[i+1]).strip(), "Points": str(l[i+2]).strip(), "Tourns": str(l[i+3]).strip(), "Rank": rank+1}
-    rank += 1
+    #print("HELLO OOOO " + str(data))
+    h = html2text.HTML2Text()
+    h.ignore_links = True
 
-#f = open('db.txt.', w)
-fjson = open("db.json", 'w')
-json.dump(rankings, fjson)
+    print(type(data))
+    l = h.handle(data).split('|')
+    l = [x for x in l if "/" not in x and "\n" not in x]
+    print(l)
+    print(len(l))
+    rankings = [{}]*100
+    rank = 0
+    lenl = len(l)
+    print(lenl)
+    for i in range(0, lenl, 5):
+        print(i)
+        rankings[rank] = {"Name": str(l[i+0]).strip(), "Age": str(l[i+1]).strip(), "Points": str(l[i+2]).strip(), "Tourns": str(l[i+3]).strip(), "Rank": rank+1}
+        rank += 1
+
+    #f = open('db.txt.', w)
+    fjson = open("db.json", 'w')
+    json.dump(rankings, fjson)
+
+def wta_rankings():
+    print("WTA Rankings")
+
+def itf_rankings():
+    print("ITF Rankings")
